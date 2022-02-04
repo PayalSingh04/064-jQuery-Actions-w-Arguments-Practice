443fa0cc63d2e2bda1d0a88d91fa9d2b9ec46685
PayalSingh04 <97962955+PayalSingh04@users.noreply.github.com>
2022-02-04 08:40:11 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+// Guided Practice
+$(".reply-yes").click(function() {  
+    $(".homers-reply").text("Woo Hoo!");
+    $(".reply-yes").css("background-color", "yellow");
+});
+
+//Independent Practice
+$(".reply-no").click(function() {  
+    $(".homers-reply").text("Doh!");
+    $(".reply-no").css("background-color","green");
+    $("img").css("height","1000px","width","1000px");
+    $("h1").css("color","blue");
+      
+    
+});
+
